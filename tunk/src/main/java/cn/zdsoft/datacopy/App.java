8381ff5dbff2ee65831eb7961299e6e8ea9c294c
duanjt1314 <479224006@qq.com>
2017-07-14 15:36:09 +0800

    调整格式，增加注释

--- tunk/src/main/java/cn/zdsoft/datacopy/App.java
+++ tunk/src/main/java/cn/zdsoft/datacopy/App.java
@@ -23,13 +23,13 @@ public class App {
 			// 添加程序结束监听 ，用于释放系统资源
 			Runtime.getRuntime().addShutdownHook(new Thread() {
 				public void run() {
-					startUp.Stop();//系统结束
+					startUp.Stop();// 系统结束
 				}
 			});
-			
+
 			LogHelper.getLogger().info("程序启动成功");
 		} catch (Exception ex) {
-			LogHelper.getLogger().error("程序出现未识别的异常", ex);			
+			LogHelper.getLogger().error("程序出现未识别的异常", ex);
 		}
 	}
 
